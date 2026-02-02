8d74986c233ab98f3e8861141ff8c7965b9a6d2b
SallyJBusch <160055134+SallyJBusch@users.noreply.github.com>
2026-02-02 15:55:19 -0500

    Add testbench for shift_left module

---
+++ Phase1/shift_left_tb.v
@@ -0,0 +1,35 @@
+`timescale 1ns/1ps
+
+module shift_left_tb;
+
+    reg  signed [31:0] data_in;
+    reg  signed [31:0]  shift_amount;
+    wire signed [31:0] data_out;
+
+    shift_left DUT (
+        .data_in(data_in),
+        .shift_amount(shift_amount),
+        .data_out(data_out)
+    );
+
+    initial begin
+        $dumpfile("shift_left_tb.vcd");
+        $dumpvars(0, shift_left_tb);
+
+        data_in = 0;
+        shift_amount = 0;
+        #10;   
+
+        data_in = 5; //5  in binary is 00000000000000000000000000000101
+        shift_amount = 3; //3 in binary is 00000000000000000000000000011
+        #10;    //expected output = 00000000000000000000000000101000 (40)
+
+        data_in = 6; //6 in binary is 00000000000000000000000000000110
+        shift_amount = 1; // 1 in binary is 00000000000000000000000000000001
+        #10;   //expected output = 00000000000000000000000000001100 (12)
+
+        $display("Simulation complete.");
+        $finish;
+    end
+
+endmodule
