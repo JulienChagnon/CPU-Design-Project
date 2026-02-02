1170ebbebad1f43e1096d6a8bd621b754e7af691
SallyJBusch <160055134+SallyJBusch@users.noreply.github.com>
2026-02-02 15:53:36 -0500

    Update shift_left

--- Phase1/shift_right.v
+++ Phase1/shift_right.v
@@ -1,6 +1,6 @@
 module shift_right (
     input  wire [31:0] data_in,       // Data to be shifted
-    input  wire [4:0]  shift_amount,  // Number of bits to shift
+    input  wire [31:0]  shift_amount,  // Number of bits to shift
     output wire [31:0] data_out       // Shifted output
 );
     assign data_out = data_in >> shift_amount;
